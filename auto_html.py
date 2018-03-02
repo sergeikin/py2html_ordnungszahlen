7229a6382e2ffa1c189471bec79bbfc0438de2af
Command_Crafter <dkrklksk@gmail.com>
2018-03-02 17:46:35 +0100

    Added auto-generated table up to tenth to auto_html.py.

--- auto_html.py
+++ auto_html.py
@@ -8,12 +8,28 @@ def html_head():
 </head>
 <body>
 <h3>The table of enumerations from 1st to 1,000,000th</h3>
+###UNDER CONSTRUCTION!###<br>
 <table border="1" width="1000">
 <tbody>"""
     return text2html
 
 def html_body():
-    text2html = """ ###UNDER CONSTRUCTION!### """
+    text2html = """ """
+    text2html = text2html + """<tr>"""
+    for i in range(10):
+        text2html = text2html + """<td>"""
+        j = i+1
+        text2html = text2html + str(j)
+        if j == 1:
+            text2html = text2html +"st"
+        elif j == 2:
+            text2html = text2html +"nd"
+        elif j == 3:
+            text2html = text2html +"rd"
+        else:
+            text2html = text2html +"th"
+        text2html = text2html + """</td>"""
+    text2html = text2html + """</tr>"""
     return text2html
 
 def html_foot():
